d506f868db8887a4bd819d708a7c6848e4939c96
liuweibo <liuwb@youxuepai.com>
2019-08-19 18:00:58 +0800

    初始化

---
+++ src/main/java/com/lwb/easy/excel/constant/MediaType.java
@@ -0,0 +1,13 @@
+package com.lwb.easy.excel.constant;
+
+/**
+ * 媒体类型
+ * @author liuweibo
+ * @date 2019/8/19
+ */
+public interface MediaType {
+    /**
+     * 流类型，用于下载是设置在header中
+     */
+    String APPLICATION_OCTET_STREAM_VALUE = "application/octet-stream";
+}
